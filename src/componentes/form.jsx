04bbb2626058107a4559a1ab29ba6e2684a1ec28
ana <anamorais.dev@gmail.com>
2023-06-27 17:11:12 -0300

    salvando valor dos inputs nos states

--- src/componentes/form.jsx
+++ src/componentes/form.jsx
@@ -6,30 +6,38 @@ const Form = () => {
     const [categoria, setCategoria] = useState()
     const [prazo, setPrazo] = useState()
 
-    return(
+    return (
         <form>
             <label>
                 Titulo
-                <input placeholder="Digite um titulo para sua tarefa"></input>
+                <input 
+                    placeholder="Digite um titulo para sua tarefa" 
+                    value={titulo} 
+                    onChange={(event) => setTitulo(event.target.value)}>
+                </input>
             </label>
 
             <label>
                 Descricao
-                <textarea placeholder="Descreva sua tarefa..."></textarea>
+                <textarea 
+                    placeholder="Descreva sua tarefa..." 
+                    value={descricao} 
+                    onChange={(event) => setDescricao(event.target.value)}>
+                </textarea>
             </label>
 
             <label>
                 categoria
-                <select>
+                <select value={categoria} onChange={(event) => setCategoria(event.target.value)}>
                     <option>a</option>
                 </select>
             </label>
-            
+
             <label>
                 Prazo de entrega
-                <input type="date"/>
+                <input type="date" value={prazo} onChange={(event) => setPrazo(event.target.value)} />
             </label>
-            
+
             <button type="submit">adicionar tarefa</button>
         </form>
     )
