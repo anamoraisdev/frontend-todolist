66c235092706cbca8686a6a6e6aedbf6ce9ceba7
ana <anamorais.dev@gmail.com>
2023-06-27 17:33:38 -0300

    declarando state para as tarefas e passando pro form por meio de props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
+import { useState } from 'react';
 import './App.css';
 import Form from './componentes/form';
 
 function App() {
+  const [tarefas, setTarefas] = useState([])
+  
   return (
     <div className="App">
-      <Form/>
+      <Form setTarefas={setTarefas} tarefas={tarefas}/>
     </div>
   );
 }
