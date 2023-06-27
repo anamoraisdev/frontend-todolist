3089388b0a9f26d9bf5d0fc4d9c9d3d70581778f
ana <anamorais.dev@gmail.com>
2023-06-27 18:13:07 -0300

    dados via props e renderizando um componente cardTarefa para cada tarefa

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import { useState } from 'react';
 import './App.css';
 import Form from './componentes/form';
+import CardTarefa from './componentes/cardTarefa';
 
 function App() {
   const [tarefas, setTarefas] = useState([])
-  
+
   return (
     <div className="App">
       <Form setTarefas={setTarefas} tarefas={tarefas}/>
+      {tarefas.map((tarefa) =>
+        <CardTarefa tarefa={tarefa}/> 
+      )}
     </div>
   );
 }
--- src/componentes/cardTarefa.jsx
+++ src/componentes/cardTarefa.jsx
@@ -1,10 +1,10 @@
-const CardTarefa = () => {
+const CardTarefa = ({tarefa}) => {
     return (
         <div>
             <input type="checkbox"></input>
-            <h2>titulo da tarefa</h2>
-            <p>descricao da tarefa</p>
-            <p>prazo</p>
+            <h2>{tarefa.titulo}</h2>
+            <p>{tarefa.descricao}</p>
+            <p>{tarefa.prazo}</p>
             <button>editar tarefa</button>
             <button>excluir tarefa</button>
         </div>
