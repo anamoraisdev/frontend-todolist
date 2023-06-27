37e96536c28c826823648fab6f39a10af287d4bf
ana <anamorais.dev@gmail.com>
2023-06-27 18:07:23 -0300

    html inicial: estrutura base para receber dados da tarefa

---
+++ src/componentes/cardTarefa.jsx
@@ -0,0 +1,14 @@
+const CardTarefa = () => {
+    return (
+        <div>
+            <input type="checkbox"></input>
+            <h2>titulo da tarefa</h2>
+            <p>descricao da tarefa</p>
+            <p>prazo</p>
+            <button>editar tarefa</button>
+            <button>excluir tarefa</button>
+        </div>
+    )
+}
+
+export default CardTarefa
