c1954e1316a00337899e46c3aacb2fa722bc14b1
ana <anamorais.dev@gmail.com>
2023-07-03 16:14:41 -0300

    estilizando

--- src/componentes/cardTarefa.jsx
+++ src/componentes/cardTarefa.jsx
@@ -2,14 +2,14 @@
 const CardTarefa = ({tarefa, openModal, excluirTarefa}) => {
     
     return (
-            <main className="bg-indigo-200 shadow-lg flex p-4 m-4 justify-between rounded-md gap-3 text-slate-700">
+            <main className="bg-indigo-200 shadow-lg flex p-4 m-4 justify-between rounded-md gap-3 text-slate-700 w-[100%]">
                 <div className="flex">
                     <input type="checkbox"></input>
                     <h2>{tarefa.titulo}</h2>
                 </div>
                 <div className="flex gap-3">
-                    <button onClick={() => openModal(tarefa)} className=" px-3 bg-indigo-400 rounded-md text-white hover:bg-indigo-500">editar</button>
-                    <button onClick={() => excluirTarefa(tarefa)} className="px-3 bg-indigo-400 rounded-md text-white hover:bg-indigo-500">excluir</button>
+                    <button onClick={() => openModal(tarefa)} className=" px-3 bg-indigo-500 rounded-md text-white hover:bg-indigo-400">editar</button>
+                    <button onClick={() => excluirTarefa(tarefa)} className="px-3 bg-indigo-500 rounded-md text-white hover:bg-indigo-400">excluir</button>
                 </div>
             </main>
     )
