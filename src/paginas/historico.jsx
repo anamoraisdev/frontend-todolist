082c5cd1f64660c01ea072c40ae8e09fa6e1ddf6
ana <anamorais.dev@gmail.com>
2023-07-04 20:55:38 -0300

    funcao de reativando tarefa

--- src/paginas/historico.jsx
+++ src/paginas/historico.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react"
+
 
 const Historico = ({reativarTarefa}) => {
-    const [tarefasConcluidas , setTarefasConcluidas] = useState(JSON.parse(localStorage.getItem("tarefasConcluidas")) || [])
+    const tarefasConcluidas = JSON.parse(localStorage.getItem("tarefasConcluidas")) || []
 
     return (
-        <div>
+        <div className="mt-16">
             {tarefasConcluidas.map((tarefa) => 
                 <div>
                     <p>{tarefa.titulo}</p>
