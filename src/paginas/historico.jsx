31ef32accebdcfec215bb12b2883582fbb4475d9
ana <anamorais.dev@gmail.com>
2023-07-04 19:50:13 -0300

    estrutura base do historico, pegando tarefas do localStorage

--- src/paginas/historico.jsx
+++ src/paginas/historico.jsx
@@ -1,8 +1,14 @@
 const Historico = () => {
-    
+    const [tarefasConcluidas , setTarefasConcluidas] = useState(JSON.parse(localStorage.getItem("tarefasConcluidas")) || [])
+
     return (
         <div>
-            historico
+            {tarefasConcluidas.map((tarefa) => 
+                <div>
+                    <p>{tarefa.titulo}</p>
+                    <button onClick={() => reativarTarefa()}>reativar tarefa</button>
+                </div>
+            )}
         </div>
     )
 }
